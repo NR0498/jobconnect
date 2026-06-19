f5680d53db6627fef6ded1bd13d291cbfa7f9dda
NR0498 <nr0498@srmist.edu.in>
2026-06-19 11:00:14 +0530

    Fix Vercel ESM API module resolution

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,10 @@
 let appPromise: ReturnType<
-  typeof import("../server/app")["createApp"]
+  typeof import("../server/app.js")["createApp"]
 > | null = null;
 
 export default async function handler(req: any, res: any) {
   try {
-    appPromise ??= import("../server/app").then(({ createApp }) => createApp());
+    appPromise ??= import("../server/app.js").then(({ createApp }) => createApp());
     const app = await appPromise;
     return app(req, res);
   } catch (error) {
